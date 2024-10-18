bd8ae3ca107f32b7b52cff4d69d3a4e6b12403f4
Ridwan07nz <ridwanbtlfarid73@gmail.com>
2024-10-18 21:19:09 +0700

    [Status] Tugas sudah selesai, ini hanya bekerja dalam ukuran full desktop

--- src/main.js
+++ src/main.js
@@ -12,12 +12,17 @@ btnTambah.onclick = (e) => {
    task.id = `${Date.now()}`;
    task.className = `...`;
    task.innerHTML = `
-         <p class="...">${title}</p>
-         <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}"
-                class="...">
-         <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}"
-                class="...">
+         <div class="flex gap-20 border-2 border-style items-center px-5 bg-gradient-to-r from-bg-slate-700 via-slate-400 to-bg-slate-300 border-slate-600 h-[40px] w-full rounded-lg">
+            <p class="h-[25px] text-slate-300 justify-center text-center w-full">${title}</p>
+            <div class="flex gap-8">
+               <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}"
+                     class="w-[100px] h-[25px] bg-slate-500 text-slate-300 rounded-lg">
+               <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}"
+                     class="w-[100px] h-[25px] bg-slate-500 text-slate-300 rounded-lg">
+            </div>
+         </div>
    `;
+   task.style.opacity = "1";
    output.appendChild(task);
    input.value = "";
    e.preventDefault();
